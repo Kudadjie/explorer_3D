0b89e7b4fba5f9d60518f4b5fb44f59036609f70
John David K.T. Kudadjie <jdkudadjie@outlook.com>
2023-06-14 22:42:54 +0000

    Finalized Login UI.

--- src/app/interactive-viewer/Form.tsx
+++ src/app/interactive-viewer/Form.tsx
@@ -1,21 +1,23 @@
-import styles from "./styles.module.scss"
-import React, {useRef, useState} from "react";
-import {boolean} from "zod";
-export default function Form(){
+import styles from "./styles.module.scss";
+import React, { useState } from "react";
+export default function Form() {
+  const [isLoginPage, setIsLoginPage] = useState(true);
+  const [showPassword, setShowPassword] = useState(false);
+  const passwordRef = React.useRef<HTMLInputElement>(null);
+  const emailRef = React.useRef<HTMLInputElement>(null);
 
-    const [showPassword, setShowPassword] = useState(false)
-    const passwordRef = React.useRef<HTMLInputElement>(null);
-    const usernameRef = React.useRef<HTMLInputElement>(null)
+  function togglePasswordReveal(value: boolean) {
+    const type =
+      passwordRef.current?.getAttribute("type") === "password"
+        ? "text"
+        : "password";
+    passwordRef.current?.setAttribute("type", type);
+    setShowPassword(value);
+  }
 
-    function togglePasswordReveal(value: boolean){
-        const type = passwordRef.current?.getAttribute("type") === "password" ? "text" : "password";
-        passwordRef.current?.setAttribute("type", type);
-        setShowPassword(value)
-    }
-
-    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault()
-/*
+  const handleLoginSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    /*
 
         const response = await fetch('/api/login', {
             method: 'POST',
@@ -23,36 +25,108 @@ export default function Form(){
                 'Content-Type': 'application/json',
             },
             body: JSON.stringify({
-                username: usernameRef.current?.value,
+                email: emailRef.current?.value,
                 password: passwordRef.current?.value,
             }),
         })
 
         const result = await response.json()
         console.log(result)*/
-    }
+  };
 
-    return <form className={styles.form} onSubmit={handleSubmit}>
-        <label htmlFor="username" className={styles.usernameLabel}>Username</label>
-        <br/>
-        <input className={styles.usernameInput} ref={usernameRef} id="username" type="text" name="username" required minLength={10}  maxLength={10} placeholder="Enter your username" />
-        <br/>
-        <label htmlFor="password" className={styles.passwordLabel}>Password</label>
-        <br/>
-        <div className={styles.passwordInputContainer}>
-            <input className={styles.passwordInput} ref={passwordRef} id="password" type="password" name="password" required minLength={10} maxLength={10}  title="Password should be digits (0 to 9) or alphabets (a to z)." placeholder="Enter your password"/>
-            <div className={styles.showPasswordContainer}>
-                {showPassword && <i  className="fa-regular fa-eye fa-lg" style={{color: "#abadb0"}} onClick={()=>{
-                    togglePasswordReveal(false)
+  const handleSignupSubmit = () => {};
 
-                }}></i>}
-                {!showPassword && <i className="fa-regular fa-eye-slash fa-lg" style={{color: "#abadb0"}} onClick={()=>{
-
-                    togglePasswordReveal(true)
-                }}></i>}
-            </div>
+  return (
+    <form
+      className={styles.form}
+      onSubmit={isLoginPage ? handleLoginSubmit : handleSignupSubmit}
+    >
+      <label htmlFor="email" className={styles.emailLabel}>
+        Email
+      </label>
+      <br />
+      <input
+        className={styles.emailInput}
+        ref={emailRef}
+        id="email"
+        type="email"
+        name="email"
+        required
+        placeholder="Enter your email"
+      />
+      <br />
+      <label htmlFor="password" className={styles.passwordLabel}>
+        Password
+      </label>
+      <br />
+      <div className={styles.passwordInputContainer}>
+        <input
+          className={styles.passwordInput}
+          ref={passwordRef}
+          id="password"
+          type="password"
+          name="password"
+          required
+          title="Password should be digits (0 to 9) or alphabets (a to z)."
+          placeholder="Enter your password"
+        />
+        <div
+          className={styles.showPasswordContainer}
+          style={!isLoginPage ? { top: "59.5%" } : undefined}
+        >
+          {showPassword && (
+            <i
+              className="fa-regular fa-eye fa-lg"
+              style={{ color: "#abadb0" }}
+              onClick={() => {
+                togglePasswordReveal(false);
+              }}
+            ></i>
+          )}
+          {!showPassword && (
+            <i
+              className="fa-regular fa-eye-slash fa-lg"
+              style={{ color: "#abadb0" }}
+              onClick={() => {
+                togglePasswordReveal(true);
+              }}
+            ></i>
+          )}
         </div>
-        <br/>
-        <button className={styles.signInBtn} type="submit">Enter</button>
+      </div>
+      {isLoginPage && <a className={styles.forgotPassword}>Forgot password?</a>}
+      <br />
+      <button
+        className={styles.signInBtn}
+        type="submit"
+        style={!isLoginPage ? { marginTop: "15px" } : undefined}
+      >
+        {isLoginPage ? "Login" : "Sign Up"}
+      </button>
+
+      {isLoginPage ? (
+        <p className={styles.pageInfo}>
+          Don&apos;t have an account yet?{" "}
+          <a
+            onClick={() => {
+              setIsLoginPage(false);
+            }}
+          >
+            Sign up
+          </a>
+        </p>
+      ) : (
+        <p className={styles.pageInfo}>
+          Already have an account?{" "}
+          <a
+            onClick={() => {
+              setIsLoginPage(true);
+            }}
+          >
+            Login
+          </a>
+        </p>
+      )}
     </form>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,80 +1,105 @@
-"use client"
-import styles from "./styles.module.scss"
-import { useState, useEffect } from 'react';
+"use client";
+import styles from "./styles.module.scss";
+import { useState, useEffect } from "react";
 import Image from "next/image";
-import warningIcon from "../assets/warning-icon.svg"
-import logo from "../assets/logo/logo.svg"
-import desktopBackground from "../assets/Hero and Backdrop/hero-desktop.svg"
-import tabletBackground from "../assets/Hero and Backdrop/hero-tablet.svg"
-import mobileBackground from "../assets/Hero and Backdrop/hero-mobile.svg"
+import warningIcon from "../assets/warning-icon.svg";
+import logo from "../assets/logo/logo.svg";
+import desktopBackground from "../assets/Hero and Backdrop/hero-desktop.svg";
 import Form from "@/app/interactive-viewer/Form";
 
-
 export default function Home() {
+  const [loaded, setLoaded] = useState(false);
+  const size = displayBackground(useWindowSize());
   return (
     <main className={styles.main}>
-        <div className={styles.background}>
-            {(displayBackground(useWindowSize()) === "mobile")  && <Image src={mobileBackground} alt="#" className={styles.backgroundImage} priority/>}
-            {(displayBackground(useWindowSize()) === "tablet")  && <Image src={tabletBackground} alt="#" className={styles.backgroundImage} priority/>}
-            {(displayBackground(useWindowSize()) === "desktop") && <Image src={desktopBackground} alt="#" className={styles.backgroundImage} priority />}
-        </div>
-        <div className={styles.container}>
-          <Image src={warningIcon} className={styles.warningIcon} alt="#"/>
-            <Image src={logo} className={styles.logoTop} alt="3d explorer logo" priority/>
-          <p className={styles.warningText}>The 3D Explorer project is best viewed on a tablet or desktop device. Your current screen size is not optimal.
-              Please change devices and try again.</p>
-            <Form/>
-          <Image src={logo} className={styles.logoBottom} alt="3d explorer logo"/>
-        </div>
-    </main>
-  )
-}
+      <div className={styles.background}>
+        {/* {displayBackground(useWindowSize()) === "desktop" && ( */}
 
+        {(size === "desktop" || size === "tablet") && (
+          <Image
+            src={desktopBackground}
+            alt="#"
+            className={styles.backgroundImage}
+            priority
+          />
+        )}
+        {/* {displayBackground(useWindowSize()) === "mobile" && (
+          <Image
+            src={mobileBackground}
+            alt="#"
+            className={styles.backgroundImage}
+            priority
+          />
+        )} */}
+        <div className={styles.loadingBackground}></div>
+      </div>
+      <div className={styles.container}>
+        <Image src={warningIcon} className={styles.warningIcon} alt="#" />
+        <Image
+          src={logo}
+          className={styles.logoTop}
+          alt="3d explorer logo"
+          priority
+        />
+        <p className={styles.warningText}>
+          The 3D Explorer project is best viewed on a tablet or desktop device.
+          Your current screen size is not optimal. Please change devices and try
+          again.
+        </p>
+        <Form />
+        <Image
+          src={logo}
+          className={styles.logoBottom}
+          alt="3d explorer logo"
+        />
+      </div>
+    </main>
+  );
+}
 
 // Hook
 type windowSizeType = {
-    width: number,
-    height: number
+  width: number;
+  height: number;
+};
+function useWindowSize(): windowSizeType {
+  // Initialize state with undefined width/height so server and client renders match
+  // Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
+  const [windowSize, setWindowSize] = useState({
+    width: 0,
+    height: 0,
+  });
+
+  useEffect(() => {
+    // only execute all the code below in client side
+    // Handler to call on window resize
+    function handleResize() {
+      // Set window width/height to state
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    }
+
+    // Add event listener
+    window.addEventListener("resize", handleResize);
+
+    // Call handler right away so state gets updated with initial window size
+    handleResize();
+
+    // Remove event listener on cleanup
+    return () => window.removeEventListener("resize", handleResize);
+  }, []); // Empty array ensures that effect is only run on mount
+  return windowSize;
 }
-function useWindowSize():windowSizeType {
-    // Initialize state with undefined width/height so server and client renders match
-    // Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
-    const [windowSize, setWindowSize] = useState({
-        width: 0,
-        height: 0,
-    });
-
-    useEffect(() => {
-        // only execute all the code below in client side
-        // Handler to call on window resize
-        function handleResize() {
-            // Set window width/height to state
-            setWindowSize({
-                width: window.innerWidth,
-                height: window.innerHeight,
-            });
-        }
-
-        // Add event listener
-        window.addEventListener("resize", handleResize);
-
-        // Call handler right away so state gets updated with initial window size
-        handleResize();
-
-        // Remove event listener on cleanup
-        return () => window.removeEventListener("resize", handleResize);
-    }, []); // Empty array ensures that effect is only run on mount
-    return windowSize;
-}
-function displayBackground(object:windowSizeType){
-    //mobile
-    if(object.width >= 1200) {
-        return "desktop"
-    }
-    if(object.width >= 426 && object.width <= 1200){
-        return "tablet"
-    }
-    else {
-        return "mobile"
-    }
+function displayBackground(object: windowSizeType) {
+  //mobile
+  if (object.width >= 1200) {
+    return "desktop";
+  }
+  if (object.width >= 426 && object.width <= 1200) {
+    return "tablet";
+  } else {
+    return "mobile";
+  }
 }
