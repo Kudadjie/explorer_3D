f621b00ba14a8d4a7ffb8d0ce25bcfb2ff6367af
John David K.T. Kudadjie <jdkudadjie@outlook.com>
2023-09-16 21:30:44 +0000

    reduced home background image res

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import styles from "./styles.module.scss";
 import React from "react";
 import LoadingView from "@/components/LoadingView";
-import background from "../../public/assets/Hero and Backdrop/home_background.png";
+import background from "../../public/assets/Hero and Backdrop/home_background_min.png";
 import Image from "next/image";
 
 export default function Home() {
